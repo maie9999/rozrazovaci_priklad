8a26272132f1d8d0d4cd854c4284f194c7f248ec
maie9999 <jezkova.nikca@gmail.com>
2017-03-30 11:28:36 +0200

    LEVEL 3- working URL
    - parameter x 
    - resolved problem with JSP - ( new version of condition)

--- priklad1/src/com/rp/Servlet1.java
+++ priklad1/src/com/rp/Servlet1.java
@@ -6,23 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Servlet1 extends HttpServlet{
+public class Servlet1 extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-	throws IOException{
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
-		 String outp= request.getParameter("output");
-		PrintWriter out = response.getWriter();
-		out.println(out);
-		
-	  
-}
-		
-
-
-
-		
-	
 	}
-	
-	
+
+}
